961173f4e806ffa4f742e34250fad51ea4e4791a
Amit Goyal <agoyal3_be21@thapar.edu>
2023-03-17 11:07:20 +0530

    Fan-in and timeout logic added

---
+++ fan_in.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+func Funnel(sources ...<-chan int) <-chan int {
+	dest := make(chan int)
+
+	wg := &sync.WaitGroup{}
+
+	wg.Add(len(sources))
+
+	for _, ch := range sources {
+		go func(c <-chan int) {
+			defer wg.Done()
+			for n := range c {
+				dest <- n
+			}
+		}(ch)
+	}
+	go func() {
+		wg.Wait()
+		close(dest)
+	}()
+	return dest
+}
+
+func main() {
+	sources := make([]<-chan int, 0)
+	for i := 0; i < 3; i++ {
+		ch := make(chan int)
+		sources = append(sources, ch)
+
+		go func() {
+			defer close(ch)
+			for i := 0; i < 5; i++ {
+				ch <- i
+				time.Sleep(time.Second)
+			}
+		}()
+	}
+	// fmt.Println(sources)
+	dest := Funnel(sources...)
+	for d := range dest {
+		fmt.Println(d)
+	}
+}
